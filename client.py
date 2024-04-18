c62dadfa31614f10e12b150f2c95fb9c7b328856
ILUNOSHA~ <dimakorolev2000@gmail.com>
2024-04-18 20:42:26 +0300

    Diffie-Hellman encryption

--- client.py
+++ client.py
@@ -7,8 +7,14 @@ PORT = 8080
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
-A = g ** a % p
+p, g, a = 23, 5, 6
+A = (g ** a) % p
 sock.send(pickle.dumps((p, g, A)))
+
+msg = pickle.loads(sock.recv(1024))
+B = msg
+
+K = (B ** a) % p
+print("Client's calculated key:", K)
 
 sock.close()
--- server.py
+++ server.py
@@ -9,7 +9,14 @@ sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+msg = pickle.loads(conn.recv(1024))
+p, g, A = msg
+
+b = 15
+B = (g ** b) % p
+conn.send(pickle.dumps(B))
+
+K = (A ** b) % p
+print("Server's calculated key:", K)
 
 conn.close()
